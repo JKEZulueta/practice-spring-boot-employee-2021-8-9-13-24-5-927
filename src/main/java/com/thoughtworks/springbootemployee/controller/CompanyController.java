e64cda83eef8014a547ee445ac227247e16898be
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-12 00:11:16 +0800

    [STSpringBoot][Robert&Kyle] feat: Added getAllCompany and lists all companies

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -1,7 +1,25 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.model.Company;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/companies")
 public class CompanyController {
+    private final List<Company> companies = new ArrayList<>();
 
+    public CompanyController(){
+        companies.add(new Company(1,"KyleCompany"));
+        companies.add(new Company(2, "MagicCompany"));
+    }
 
-
+    @GetMapping
+    public List<Company> getAllCompany(){
+        return companies;
+    }
 }
