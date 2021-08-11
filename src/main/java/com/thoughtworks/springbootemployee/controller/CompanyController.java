db189201459d72fbc355d68231e63c8a7843cb46
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-12 00:34:04 +0800

    [STSpringBoot][Robert&Kyle] feat: Added getCompanyById to return company based on ID

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Company;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,13 @@ public class CompanyController {
     @GetMapping
     public List<Company> getAllCompany(){
         return companies;
+    }
+
+    @GetMapping(path = "/{companyId}")
+    public Company getCompanyById(@PathVariable Integer companyId){
+        return companies.stream()
+                .filter((company -> company.getCompanyId().equals(companyId)))
+                .findFirst()
+                .orElse(null);
     }
 }
