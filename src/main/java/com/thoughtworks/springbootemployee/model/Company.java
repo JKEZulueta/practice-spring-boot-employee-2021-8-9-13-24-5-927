f6438de8152cd5caea1668626bafd6c9bb866e7f
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-11 23:55:18 +0800

    [STSpringBoot][Robert&Kyle] feat: Added constructors and getters for company model

--- src/main/java/com/thoughtworks/springbootemployee/model/Company.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Company.java
@@ -1,5 +1,22 @@
 package com.thoughtworks.springbootemployee.model;
 
 public class Company {
+    private Integer companyId;
+    private String companyName;
 
+    public Company(){
+
+    }
+    public Company(Integer companyId, String companyName) {
+        this.companyId = companyId;
+        this.companyName = companyName;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
 }
