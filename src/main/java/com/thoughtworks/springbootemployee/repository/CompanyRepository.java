78dde4a3319b9e8c77dfd2a3fcab2ef6f134ffaa
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-12 10:20:36 +0800

    [STSpringBoot][Robert&Kyle] feat: added company repository containing employee list

--- src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
@@ -1,16 +1,24 @@
 package com.thoughtworks.springbootemployee.repository;
 
+import com.thoughtworks.springbootemployee.controller.CompanyController;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Repository
 public class CompanyRepository {
     private List<Company> companies = new ArrayList<>();
+
+    public CompanyRepository(){
+        companies.add(new Company(1, "KyleCompany", Arrays
+                .asList(new Employee(1, "Kyle", 23, "Male", 3000),
+                        new Employee(2, "Robert", 23, "Male", 5000))));
+    }
 
     public List<Company> getAllCompany(){
         return companies;
