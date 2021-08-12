fe8581f5ce0799cc6cb96b48aa0c29a99c83912b
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-12 09:54:11 +0800

    [STSpringBoot][Robert&Kyle] feat: added employee list into the company model

--- src/main/java/com/thoughtworks/springbootemployee/model/Company.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Company.java
@@ -1,18 +1,25 @@
 package com.thoughtworks.springbootemployee.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Company {
     private Integer companyId;
     private String companyName;
-    private Integer employeeId;
-    private String employeeName;
+    private List<Employee> employees = new ArrayList<>();
 
-    public Company(Integer companyId, String companyName) {
+    public Company(Integer companyId, String companyName, List<Employee> employees) {
         this.companyId = companyId;
         this.companyName = companyName;
+        this.employees = employees;
     }
 
     public Company(){
 
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
     }
 
     public Integer getCompanyId() {
