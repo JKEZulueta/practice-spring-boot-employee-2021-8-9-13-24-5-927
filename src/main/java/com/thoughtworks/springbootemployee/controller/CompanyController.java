db8b565bd2aa2a4434d5aa1c6b5970ee0e97a738
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-12 09:29:39 +0800

    [STSpringBoot][Robert&Kyle] refact: Changed company controller to use company service.

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.service.CompanyService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,24 +13,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
-    private final List<Company> companies = new ArrayList<>();
+    private final CompanyService companyService;
 
-    public CompanyController(){
-        companies.add(new Company(1,"KyleCompany"));
-        companies.add(new Company(2, "MagicCompany"));
-        companies.add(new Company(3, "WishWashCompany"));
+    public CompanyController(CompanyService companyService){
+        this.companyService = companyService;
     }
 
     @GetMapping
     public List<Company> getAllCompany(){
-        return companies;
+        return companyService.getAllCompany();
     }
 
     @GetMapping(path = "/{companyId}")
     public Company getCompanyById(@PathVariable Integer companyId){
-        return companies.stream()
-                .filter((company -> company.getCompanyId().equals(companyId)))
-                .findFirst()
-                .orElse(null);
+        return companyService.getCompanyById(companyId);
     }
+
+
+
 }
