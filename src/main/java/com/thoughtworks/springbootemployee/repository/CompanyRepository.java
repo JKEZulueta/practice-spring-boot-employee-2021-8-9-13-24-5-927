0c3fdcb2f1c481510aed88c2c8b5c31da7271a5e
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-12 20:18:00 +0800

    [STSpringBoot][Robert&Kyle] refact: changed the companyrepo to Retiring and added a new Company repository using JPA

--- src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
@@ -1,39 +1,8 @@
 package com.thoughtworks.springbootemployee.repository;
 
-import com.thoughtworks.springbootemployee.controller.CompanyController;
 import com.thoughtworks.springbootemployee.model.Company;
-import com.thoughtworks.springbootemployee.model.Employee;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Repository
-public class CompanyRepository {
-    private List<Company> companies = new ArrayList<>();
-
-    public CompanyRepository(){
-        companies.add(new Company(1, "KyleCompany", Arrays
-                .asList(new Employee(1, "Kyle", 23, "Male", 3000),
-                        new Employee(2, "Robert", 23, "Male", 5000))));
-    }
-
-    public List<Company> getAllCompany(){
-        return companies;
-    }
-
-    public Company getCompanyById(Integer companyId){
-        return companies.stream()
-                .filter(company -> company.getCompanyId().equals(companyId))
-                .findFirst()
-                .orElse(null);
-    }
-
-    public Company addCompany(Company company) {
-        companies.add(company);
-        return company;
-    }
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
 
 }
---
+++ src/main/java/com/thoughtworks/springbootemployee/repository/RetiringCompanyRepository.java
@@ -0,0 +1,34 @@
+package com.thoughtworks.springbootemployee.repository;
+
+import com.thoughtworks.springbootemployee.controller.CompanyController;
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class RetiringCompanyRepository {
+    private List<Company> companies = new ArrayList<>();
+
+    public RetiringCompanyRepository(){
+        companies.add(new Company(1, "KyleCompany", Arrays
+                .asList(new Employee(1, "Kyle", 23, "Male", 3000),
+                        new Employee(2, "Robert", 23, "Male", 5000))));
+    }
+
+    public List<Company> getAllCompany(){
+        return companies;
+    }
+
+    public Company getCompanyById(Integer companyId){
+        return companies.stream()
+                .filter(company -> company.getCompanyId().equals(companyId))
+                .findFirst()
+                .orElse(null);
+    }
+
+}
