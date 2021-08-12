29a3ad878314af05d215de4c396d545fec7a589e
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-12 22:27:58 +0800

    [STSpringBoot][Robert&Kyle] refact: Removed unused codes.

--- src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
@@ -1,12 +1,13 @@
 package com.thoughtworks.springbootemployee.repository;
 
 import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
+
     public Company findAllByCompanyId(Integer companyId);
 
-    List<Company> findAll();
 }
