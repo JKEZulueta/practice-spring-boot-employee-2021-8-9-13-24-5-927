5fc9b6defd5b8962c46a71fa6dbcb62b812fc634
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-12 09:20:11 +0800

    [STSpringBoot][Robert&Kyle] feat: added repository to get all company and get company by Id

--- src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
@@ -1,4 +1,24 @@
 package com.thoughtworks.springbootemployee.repository;
 
+import com.thoughtworks.springbootemployee.model.Company;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class CompanyRepository {
+    private List<Company> companies = new ArrayList<>();
+
+    public List<Company> getAllCompany(){
+        return companies;
+    }
+
+    public Company getCompanyById(Integer companyId){
+        return companies.stream()
+                .filter(company -> company.getCompanyId().equals(companyId))
+                .findFirst()
+                .orElse(null);
+    }
+
 }
